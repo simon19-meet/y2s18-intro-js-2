21db82995da5dd388e11d95a15348c16ac21079a
simon19-meet <simon19@meet.mit.edu>
2018-08-01 18:32:04 +0300

    Events in process

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,5 @@
-// Put all your JavaScript in this file!
+function ChangeBgColor(object){
+	var a=prompt("Type a color");
+	var b=document.getElementsByTagName("button")[0].addEventListener("click",function(event){event.getElementsById('btn')});
+	
+}
